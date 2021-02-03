0a7389e265b0a31040494dac2b0b9d014abd3563
kishan9192 <mishra.kishan1@gmail.com>
2021-02-03 17:17:02 +0530

    prefilled forms : usermodel

--- tdf/src/app/app.component.ts
+++ tdf/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'tdf';
   public topics = ['Angular', 'React', 'Vue'];
+
+  /* having a userModel. We can consider instance like autofilled form when we want to change/edit
+     the shipment details or address while buying something, Data that already appears "prefilled" in the form*/
+  userModel  = new User('Rob', 'rob@gmail.com', 5454544, 'Vue', 'morning', true);
 }
