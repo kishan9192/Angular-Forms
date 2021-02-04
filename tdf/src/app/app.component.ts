6eee1a4a50534bfc6a9de0730132349cee144729
kishan9192 <mishra.kishan1@gmail.com>
2021-02-04 22:30:28 +0530

    final validation

--- tdf/src/app/app.component.ts
+++ tdf/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EnrollmentService } from './enrollment.service';
 import { User } from './user';
 
 @Component({
@@ -10,7 +11,31 @@ export class AppComponent {
   title = 'tdf';
   public topics = ['Angular', 'React', 'Vue'];
 
+  public topicHasError = true;
   /* having a userModel. We can consider instance like autofilled form when we want to change/edit
      the shipment details or address while buying something, Data that already appears "prefilled" in the form*/
   userModel  = new User('', 'rob@gmail.com', 5454544, 'default', 'morning', true);
+
+  public validateTopic(value: string) {
+    if (value === 'default') {
+      this.topicHasError = true;
+    }
+    else {
+      this.topicHasError = false;
+    }
+  }
+
+  constructor(public enrollmentservice: EnrollmentService) { }
+
+  onSubmit() {
+    console.log(this.userModel);
+
+    this.enrollmentservice.enroll(this.userModel)
+    .subscribe(
+      (data: any) => console.log('success', data),
+      (error: any) => console.error('Error!', error)
+    )
+  }
+
+  
 }
